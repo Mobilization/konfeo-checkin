83590f071bba00c0110959fa3cf9a5ecb9dd4c48
Marek Defeciński <marekdef@gmail.com>
2017-11-10 22:01:00 +0100

    Added normalized last and first name

--- app/src/main/java/pl/mobilization/konfeo/checkin/entities/Attendee.kt
+++ app/src/main/java/pl/mobilization/konfeo/checkin/entities/Attendee.kt
@@ -1,7 +1,9 @@
 package pl.mobilization.konfeo.checkin.entities
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Ignore
 import android.arch.persistence.room.PrimaryKey
+import org.apache.commons.lang3.StringUtils
 
 /**
  * Created by defecins on 07/11/2017.
@@ -16,4 +18,22 @@ data class Attendee(
         var group: String,
         var number: Long?,
         var needs_update: Boolean = false
-)
+) {
+    @Ignore
+    private var _last_name_normalized: String = ""
+    val last_name_normalized : String
+    get() {
+        if(_last_name_normalized == null)
+            _last_name_normalized = StringUtils.stripAccents(last_name)
+        return _last_name_normalized
+    }
+
+    @Ignore
+    private var _first_name_normalized: String = ""
+    val first_name_normalized : String
+        get() {
+            if(_first_name_normalized == null)
+                _first_name_normalized = StringUtils.stripAccents(last_name)
+            return _first_name_normalized
+        }
+}
