9ed0e312c2ac10273cbdaaf78668ca920200e64f
Marek Defeciński <marekdef@gmail.com>
2017-11-10 22:06:08 +0100

    Search capability

--- app/src/main/java/pl/mobilization/konfeo/checkin/AttendeesActivity.kt
+++ app/src/main/java/pl/mobilization/konfeo/checkin/AttendeesActivity.kt
@@ -1,6 +1,6 @@
 package pl.mobilization.konfeo.checkin
 
-import android.arch.persistence.room.Database
+import android.app.SearchManager
 import android.arch.persistence.room.Room
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +11,7 @@ import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -20,6 +21,11 @@ import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 import pl.mobilization.konfeo.checkin.entities.Attendee
 import pl.mobilization.konfeo.checkin.entities.AttendeeDatabase
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.widget.SearchView
+
 
 class AttendeesActivity : AppCompatActivity() {
 
@@ -49,12 +55,48 @@ class AttendeesActivity : AppCompatActivity() {
             val attendees = db.attendeeDAO().getAttendees()
 
             launch(UI) {
+//                val attendee = Attendee(Long.MAX_VALUE, "Marek", "Defeciński", "marekdef@tlen.pl", "Organizer", Integer.MAX_VALUE.toLong(), false)
+
                 attendeeAdapter.add(attendees)
+            }
+        }
+
+//        handleSearch(intent)
+    }
+
+    private fun handleSearch(intent: Intent?) {
+        intent?.let {
+            if (Intent.ACTION_SEARCH == intent.action) {
+                val query = intent.getStringExtra(SearchManager.QUERY)
+                doMySearch(query)
             }
         }
 
     }
 
+    private fun doMySearch(query: String) {
+//        attendeeAdapter.filter = query
+    }
+
+//    override fun onNewIntent(intent: Intent?) {
+//        super.onNewIntent(intent)
+//
+//        setIntent(intent)
+////        handleSearch(intent)
+//    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.app_bar_search).actionView as SearchView
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.setIconifiedByDefault(false) // Do not iconify the widget; expand it by default
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
 
     internal class AttendeesReceiver : ResultReceiver(Handler(Looper.getMainLooper())) {
         override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
