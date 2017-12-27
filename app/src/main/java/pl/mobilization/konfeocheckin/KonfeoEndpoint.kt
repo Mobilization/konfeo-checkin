180c71cd0e8c1c2681577aeeb7d5cb70bf6afe58
Marek Defeciński <marekdef@gmail.com>
2017-12-27 22:26:02 +0100

    Endpoint is not kotlin interface

---
+++ app/src/main/java/pl/mobilization/konfeocheckin/KonfeoEndpoint.kt
@@ -0,0 +1,17 @@
+package pl.mobilization.konfeocheckin
+
+import org.jsoup.nodes.Document
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+
+/**
+ * Created by marekdef on 26.12.17.
+ */
+
+interface KonfeoEndpoint {
+
+    @GET("/en/login")
+    fun login(): Call<Document>
+}
